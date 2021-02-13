26642909cf658e2dbbbb2eb9a270a57b49cbabe5
gabin.bloquet <gabin.bloquet@decathlon.com>
2021-02-13 18:24:41 +0100

    feat: add endpoint to get merchants list

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 8000;
+const merchantRouter = express.Router();
+const port = process.env.PORT || 4000;
+
+merchantRouter.route('/')
+  .get((req, res) => {
+    const response = [
+      {id: 1, name: 'La Prairie'},
+      {id: 2, name: "Prise Direct'"}
+    ]
+
+    res.json(response);
+  })
+
+app.use('/merchants', merchantRouter);
 
 app.get('/', (req, res) => {
-  res.send('Welcome to my API !')
+  res.send('Welcome to my API (powered by Nodemon) !')
 })
 
 app.listen(port, (req, res) => {

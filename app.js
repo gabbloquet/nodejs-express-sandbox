2b8c4a2ce2282ceba0d5ad26387df70d44184438
gabin.bloquet <gabin.bloquet@decathlon.com>
2021-02-13 18:26:19 +0100

    chore: rework port config

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 const merchantRouter = express.Router();
-const port = process.env.PORT || 4000;
+const classicPort = process.env.PORT || 8000;
 
 merchantRouter.route('/')
   .get((req, res) => {
@@ -20,8 +20,8 @@ app.get('/', (req, res) => {
   res.send('Welcome to my API (powered by Nodemon) !')
 })
 
-app.listen(port, (req, res) => {
-  console.log('My wonderful app is running on ' + port + ' port.')
+app.listen(classicPort, (req, res) => {
+  console.log('My wonderful app is running on ' + classicPort + ' port.')
 })
 
 

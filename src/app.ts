5500330ab456c0c2ca5cd0a5db397482a96aad47
gabin.bloquet <gabin.bloquet@decathlon.com>
2021-02-14 17:46:47 +0100

    chore: save before controller refacto

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 const express = require('express');
 import mongoose from "mongoose";
+import bodyParser from "body-parser";
 
 import * as merchantController from './domain/merchant/controllers'
 
@@ -8,8 +9,12 @@ const db = mongoose.connect('mongodb://localhost/proximityApi')
 
 const app = express();
 const classicPort = process.env.PORT || 8000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/merchants', merchantController.getMerchants);
+app.get('/merchants/:id', merchantController.getMerchantById);
+app.post('/merchants', merchantController.createMerchant);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to my API (powered by Nodemon) !')
--- src/domain/merchant/controllers/index.ts
+++ src/domain/merchant/controllers/index.ts
@@ -1,9 +1,34 @@
 import {Request, Response} from "express";
 import {Merchant, MerchantType} from "../models";
 
+/**
+ * Get all merchants.
+ * @route GET /merchants
+ */
 export const getMerchants = (req: Request, res: Response) => {
   const { query } = req;
   Merchant.find(query, (err: Error, merchants: Array<MerchantType>) => {
     return err ? res.send(err) : res.json(merchants);
   })
+};
+
+/**
+ * Get a specific merchant with ID.
+ * @route GET /merchants/:id
+ */
+export const getMerchantById = (req: Request, res: Response) => {
+  const { params } = req;
+  Merchant.findById(params.id, (err: Error, merchant: MerchantType) => {
+    return err ? res.send(err) : res.json(merchant);
+  })
+};
+
+/**
+ * Create a merchant.
+ * @route POST /merchants
+ */
+export const createMerchant = (req: Request, res: Response) => {
+  const merchant = new Merchant(req.body);
+  merchant.save();
+  return res.status(201).json(merchant)
 };
